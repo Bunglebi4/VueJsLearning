48568b50a97c7e537100dece94bc46728c743827
Bunglebi4 <102578128+Bunglebi4@users.noreply.github.com>
2023-03-20 00:26:37 +0300

    first commit

---
+++ App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import  Counter from "./components/Counter/Counter.vue";
+</script>
+
+<template>
+<div>
+  <h1> Hello </h1>
+  <Counter></Counter>
+</div>
+</template>
+<style scoped>
+h1 {
+  color: darkred;
+  text-align: center;
+}
+</style>
---
+++ components/Counter/Counter.vue
@@ -0,0 +1,30 @@
+<script setup>
+import {reactive, ref, watch} from "vue";
+const frontend = ref("Фронтдент - ")
+const counter = ref(0)
+const date = ref(new Date().toLocaleTimeString());
+
+function frontendHandler(event) {
+  frontend.value += "самое сложное направление программирования"
+}
+function counterHandler(event) {
+  console.log();
+  counter.value++;
+}
+  watch({}, () => {
+    setInterval(() => {
+      date.value = new Date().toLocaleTimeString();
+      }, 2000)
+  }, {immediate: true})
+</script>
+
+<template>
+  <div>
+  <button @click="counterHandler">counter {{ counter }}</button>
+    <button @mousemove.once="frontendHandler">  {{frontend}}</button>
+  <h1> {{ date}}</h1>
+
+  </div>
+</template>
+
+<style lang = "scss" scoped> </style>
